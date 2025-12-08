fab63963e60b7df39f697ec9e106763f52facb38
Nishanth R <nishanth.r.1207@gmail.com>
2025-12-08 22:16:40 +0530

    Array creation commands added

--- Numpy_1.py
+++ Numpy_1.py
@@ -31,4 +31,42 @@ print('Size : ',a.size)
 print('Strides : ',a.strides)            #(To go to next rows, to go to next columns)
 print('Type : ',type(a))
 
+print('-------------------------------------------------------')
+# O-D Arrays: The array that does not have any dimnension. Which consist of single value.
+a1 = np.array(12)
+print('Dimension of 0-D array : ',a1.ndim)
+print('Shape of 0-D array : ',a1.shape) 
+print('Size of 0-D array : ',a1.size) 
 
+#1-D Arrays: The array that has 0 dimensional array has its elements.
+a2 = np.array([1,2,3,4])
+print(a2)
+print('Shape of 1-D array : ',a2.shape) 
+
+#2-D Arrays: The array that has 1 dimensional array has its elements.
+a3 = np.array([[1,2,3,4],[5,6,7,8]])
+print('2-D Array \n',a3)
+print('Size of 2-D array : ',a3.size) 
+
+# Array Creation : There are multiple ways to create an array in numpy
+# 1. using List and Tuple
+a=np.array([1,2,3,4])   # Using list
+b=np.array((2,3,4))  # Using tuple
+print('Creation of array using list : ',a)
+print('Creation of array using tuple : ',b) 
+
+#2. Using zeros,ones and fixed numbers
+a1 = np.zeros((3))
+a2 = np.ones((1,3))
+a3 = np.full((2,2),9)
+print('Zero Array creation : ',a1)
+print('One Array creation : ',a2)
+print('Fixed Array creation : \n',a3)
+
+#3. Using of range,linspace and matrix
+a1 = np.eye(3,3)
+print('The array of identity matrix :\n',a1)
+a2 = np.arange(1,11,2)
+print(a2)
+a3 = np.linspace(1,10,10)
+print('The array of linspace matrix :\n',a3)
