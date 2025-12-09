96863253c7d68b907c2269858986214a1bbbc203
Nishanth R <nishanth.r.1207@gmail.com>
2025-12-09 05:49:56 +0530

    Numpy Datatypes added

--- Numpy_2.py
+++ Numpy_2.py
@@ -41,3 +41,37 @@ print(arr[::2,:])
 print(arr[1])
 print(arr[:,2])
 print(arr[:,1:3])
+
+
+print('*******************Data Types ********************')
+'''
+    1. Integer
+        a) integer (signed) : contains both positive and negative
+            1. int8 : (1 byte) (Range : -128 to 127)
+            2. int16 : (2 byte) (−32,768 to 32,767)
+            3. int32 : (4 byte) (Range : -2Billion to 2 billion)
+            4. int64 : (8 byte) (huge range)
+        b) integer(unsigned) : contains positive
+            1. uint8 : (1 byte) (Range 0 to 255) 
+            2. uint16 : (2 byte) (Range 0 to 255) 
+    
+    2. Floating point numbers:
+        1. float16 : (2 bytes)
+        2. float32 : (4 bytes)
+        3. float64 : (8 bytes)
+    
+    3. Boolean Type
+        1.True
+        2.False
+    
+    4. Complex number:
+        1.complex64 : 32bit real+32bit imaginary
+        2.complex128 : 64bit real+64bit imaginary
+    
+    5. String types: 
+        1.Unicode string :
+        2.Byte string :
+    
+    6. Object: It allows mixed type, but numpy becomes slow.
+        
+'''
